2347dc8a88d0c651876f2514a2a8ee385db6d286
Chris Gianelle <gianelc@cisco.com>
2016-09-18 17:43:00 -0400

    hello.R: updated comments and execution notes

--- hello.R
+++ hello.R
@@ -1,8 +1,16 @@
+#!/usr/bin/env Rscript
+
+# The ubiquitous hello world program 
 #   to execute, on the R console, 
 # > source("/home/gianelc/Personal/Sandbox/LearningR/hello.R")
 # > hello("Chris")
-
+#
+# or from the linux command line
+# (first chmod +x hello.R)
+# ./hello.R
 
 hello <- function(name) {
   sprintf("Hello, %s", name)
 }
+
+hello("Chris")
