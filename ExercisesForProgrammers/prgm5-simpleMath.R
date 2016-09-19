5973edf459cc9956cc342d5fcaca032c78534b99
Chris Gianelle <cgianelle@gmail.com>
2016-09-19 14:13:46 -0400

    modifed prgm5 to have some fun with R lists and lapply

--- ExercisesForProgrammers/prgm5-simpleMath.R
+++ ExercisesForProgrammers/prgm5-simpleMath.R
@@ -26,7 +26,15 @@ divideNumbers <- function(num1, num2) {
   return(paste(num1, "/", num2, "=", (num1/num2), sep=" "))
 }
 
-operations = list(addNumbers, subtractNumbers, multiplyNumbers, divideNumbers)
+operations <- list(addNumbers, subtractNumbers, multiplyNumbers, divideNumbers)
+
+runOperations <- function(opList, num1, num2) {
+  resultsList = list()
+  for (op in opList) {
+    resultsList = c(resultsList, op(num1,num2))
+  }
+  return (resultsList)
+}
 
 main <- function() {
   firstNum <- getUserInput("What is the first number? ")
@@ -40,9 +48,11 @@ main <- function() {
       # displayOutput(subtractNumbers(num1, num2))
       # displayOutput(multiplyNumbers(num1, num2))
       # displayOutput(divideNumbers(num1, num2))
-      for (i in operations) {
-        displayOutput(i(num1,num2))
-      }
+      results = runOperations(operations,num1, num2)
+      lapply(results, displayOutput)
+      # for (i in operations) {
+      #   displayOutput(i(num1,num2))
+      # }
     } else {
       print("Input was invalid; Expecting integers.")
     }
